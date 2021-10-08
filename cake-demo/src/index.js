ffca4fe255b201642fe3cecd60dd5708665a308d
Kanishk <kanishkrawat67@gmail.com>
2021-10-08 13:28:47 +0530

    addded redux-logger applymiddleware

--- cake-demo/src/index.js
+++ cake-demo/src/index.js
@@ -5,13 +5,16 @@ import { combineReducers, createStore } from 'redux';
 import App from './App';
 import { cakeReducers } from './redux/cake/cakeReducers';
 import { icecreamReducers } from './redux/cake/icecream/icecreamReducers';
+import { applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 
+const logger = createLogger();
 const rootReducer  = combineReducers ({
   cakes : cakeReducers,
   icecreams : icecreamReducers
 }) 
-const store = createStore(rootReducer);
-
+const store = createStore(rootReducer,applyMiddleware(logger));
+console.log(store.getState())
 //creation of store that takes reducer in arguments
 
 // To make our redux-store available to our react application we will use <Provider /> component , it basically makes our
